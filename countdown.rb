89eab8e5519a2ff1fbfa2a1edf7d85479d153320
Kevin Britt <kbritt510@gmail.com>
2020-07-28 20:08:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,11 @@ end
 end
 
 def countdown_with_sleep (integer)
-  
-    
+    while integer > 0
+  puts "#{integer} SECOND(S)!"
+  integer -= 1 sleep.1
+  if integer == 0
+    return "HAPPY NEW YEAR!"
+end
+end
 end
